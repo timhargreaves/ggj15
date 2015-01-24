07011dd49bdd656e86287618998d8da90c75f8e3
Tim Hargreaves <timhargreaves@jag.net>
2015-01-23 22:28:16 -0800

    Fly Out Proof of Concept
    Choices are in scene and fly out to destination

---
+++ UnityClient/Assets/Scripts/ChoiceController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoiceController : MonoBehaviour {
+
+	public GameObject choice1;
+	public GameObject choice2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityClient/Assets/Scripts/DisplayChoice.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayChoice : MonoBehaviour {
+
+	public float displayTimeDelay;
+	public Vector3 endingPosition;
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, endingPosition, velocity * Time.deltaTime);
+	}
+}
