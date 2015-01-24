3f41d35b43ae03e92ac7b0e55e0d1e82d2009456
Tim Hargreaves <timhargreaves@jag.net>
2015-01-23 21:18:24 -0800

    Falling Square
    Added falling square to bungee scene to simulate falling

---
+++ UnityClient/Assets/Scripts/Fall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 frameVelocity = velocity * Time.deltaTime;
+		transform.Translate(frameVelocity);
+	}
+}
