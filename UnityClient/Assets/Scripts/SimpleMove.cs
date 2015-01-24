2b2f905deab6e4c8afd64fe71ad9d620d6f800cd
Tim Hargreaves <timhargreaves@jag.net>
2015-01-24 14:33:27 -0800

    Updated physics and added simple mover

---
+++ UnityClient/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(velocity * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		velocity = Vector3.zero;
+	}
+}
